11d3ddb609b855d2a489c1c9d3246d7371af8e9e
Adrian Peterson Co <adrianpco@gmail.com>
2016-03-03 07:46:37 +0800

    Adds `sanitize` method for cleaning up the text when evaluating
    - Also:
      - Updates model validations syntax
    
    - Gems added:
      - humanize

--- rails/app/models/question.rb
+++ rails/app/models/question.rb
@@ -1,10 +1,25 @@
 class Question < ActiveRecord::Base
 
-  validates_presence_of :question
-  validates_presence_of :answer
+  validates :question, :answer, presence: true
 
   def is_correct?(submission)
-    answer == submission
+    sanitize(answer) == sanitize(submission)
+  end
+
+private
+
+  def sanitize(text)
+    convert_number_to_words text.gsub(/\s+/, " ").strip.downcase
+  end
+
+  def convert_number_to_words(text)
+    has_exclusive_numbers_only?(text) ? 
+      text.to_i.humanize :
+      text
+  end
+
+  def has_exclusive_numbers_only?(text)
+    (text =~ /^\d*[^a-zA-Z]$/).present?
   end
 
 end

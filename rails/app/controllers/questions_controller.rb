d12e106be516856b054fc9454cda4f457b078033
Adrian Peterson Co <adrianpco@gmail.com>
2016-03-03 08:07:32 +0800

    Finishes the functionality of answering questions.
    - Also Changes/Adds the following:
      - Views
        - `.well` class override
        - `_messages` partial for handling success and errors alerts
        - Blank slate when there are no answers
        - Centers all buttons for uniformity
      - Gems
        - `humanize` gem since I forgot to add in the last commit

--- rails/app/controllers/questions_controller.rb
+++ rails/app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_filter :find_question, only: [:show, :edit, :update]
+  before_filter :find_question, only: [:show, :edit, :update, :destroy, :answer]
 
   def index
     @questions = Question.all
@@ -12,7 +12,7 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new question_params
     if @question.save
-      redirect_to root_path, notice: 'Question created successfully.'
+      redirect_to root_path, notice: "Question created successfully."
     else
       render :new
     end
@@ -20,17 +20,28 @@ class QuestionsController < ApplicationController
 
   def update
     if @question.update_attributes question_params
-      redirect_to root_path, notice: 'Question saved successfully.'
+      redirect_to root_path, notice: "Question updated successfully."
     else
       render :edit
     end
   end
 
   def answer
-    # TODO
+    if @question.is_correct?(answer_params[:answer])
+      redirect_to root_path, notice: "You got the correct answer!"
+    else
+      flash[:error] = "Sorry, but that's the wrong answer. Please try again."
+      render :show
+    end
   end
 
-  private
+  def destroy
+    @question.destroy
+
+    redirect_to root_path, notice: "Question deleted successfully."
+  end
+
+private
 
   def find_question
     @question = Question.find(params[:id])
@@ -38,5 +49,9 @@ class QuestionsController < ApplicationController
 
   def question_params
     params.require(:question).permit(:question, :answer)
+  end
+
+  def answer_params
+    params.require(:answer).permit(:answer)
   end
 end
